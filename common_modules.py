3bdc822218dc57388b370a89ef54d84577e5bbfb
Ryan Adiputra <ryanadiputra230798@gmail.com>
2025-11-30 22:49:07 +0100

    add common modules

--- common_modules.py
+++ common_modules.py
@@ -1,16 +1,19 @@
-import os
-import sys
-import subprocess
-import glob
+import os, sys, glob
 import shutil
 import time
 import xml.etree.ElementTree as ET
 
 import numba as nb
+from numba.typed import Dict, List
+from numba.types import unicode_type, int64, float32, float64
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import meshio
+from mpl_toolkits.mplot3d import Axes3D
+
 
 # testing 
 import unittest
